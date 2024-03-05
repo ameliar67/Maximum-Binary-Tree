faacb14b8151b5cbf294c97b99f20aa8e4707f3c
Amelia <ameliar67@hotmail.com>
2024-03-04 21:30:35 -0800

    Prettier and ESLint

--- solution.js
+++ solution.js
@@ -3,17 +3,16 @@
  * @return {[number, number]}
  */
 function getMaxValueAndIndex(arr) {
-
-    let maxValue = Number.NEGATIVE_INFINITY;
-    let maxIndex = -1;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > maxValue) {
-            maxValue = arr[i];
-            maxIndex = i;
-        }
+  let maxValue = Number.NEGATIVE_INFINITY;
+  let maxIndex = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+      maxIndex = i;
     }
+  }
 
-    return [maxValue, maxIndex];
+  return [maxValue, maxIndex];
 }
 
 /**
@@ -28,37 +27,41 @@ function getMaxValueAndIndex(arr) {
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var constructMaximumBinaryTree = function(nums) {
+const constructMaximumBinaryTree = function (nums) {
+  let currMax = getMaxValueAndIndex(nums)[0];
+  let positionMax = getMaxValueAndIndex(nums)[1];
 
-    let currMax = getMaxValueAndIndex(nums)[0];
-    let positionMax = getMaxValueAndIndex(nums)[1]
+  const l = nums.slice(0, positionMax);
+  const ri = nums.slice(positionMax + 1, nums.length);
 
-    let l = nums.slice(0, positionMax);
-    let ri = nums.slice((positionMax + 1), nums.length);
+  const r = new TreeNode(currMax);
 
-    let r = new TreeNode(currMax)
+  function dfs(root, left, right) {
+    if (left.length >= 1) {
+      currMax = getMaxValueAndIndex(left)[0];
+      positionMax = getMaxValueAndIndex(left)[1];
 
-    function dfs(root, left, right)  {
-
-        if(left.length >= 1)   {
-            currMax = getMaxValueAndIndex(left)[0];
-            positionMax = getMaxValueAndIndex(left)[1];
-
-            root.left = new TreeNode(currMax);
-            dfs(root.left, left.slice(0, positionMax), left.slice(positionMax + 1, left.length));
-        }
-
-        if(right.length >= 1)   {
-            currMax = getMaxValueAndIndex(right)[0];
-            positionMax = getMaxValueAndIndex(right)[1];
-
-            root.right = new TreeNode(currMax);
-            dfs(root.right, right.slice(0, positionMax), right.slice(positionMax + 1, right.length))
-        }
-        
-        
+      root.left = new TreeNode(currMax);
+      dfs(
+        root.left,
+        left.slice(0, positionMax),
+        left.slice(positionMax + 1, left.length),
+      );
     }
 
-    dfs(r, l, ri)
-    return r
+    if (right.length >= 1) {
+      currMax = getMaxValueAndIndex(right)[0];
+      positionMax = getMaxValueAndIndex(right)[1];
+
+      root.right = new TreeNode(currMax);
+      dfs(
+        root.right,
+        right.slice(0, positionMax),
+        right.slice(positionMax + 1, right.length),
+      );
+    }
+  }
+
+  dfs(r, l, ri);
+  return r;
 };
